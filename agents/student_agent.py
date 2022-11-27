b7fcbfcc29e269430d9b5f0729624d034f0af8b2
Yi Fan Li <yifanli123@gmail.com>
2022-11-26 19:52:47 -0500

    Update student_agent.py

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -35,7 +35,7 @@ class StudentAgent(Agent):
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
 
-
+        #move selection
         moving_agent_parent = deepcopy(my_pos)
 
         step_left = max_step
@@ -49,56 +49,48 @@ class StudentAgent(Agent):
                 d_x = self.move_map[direction][0]
                 d_y = self.move_map[direction][1]
                 
-                moving_agent_child = (s_x - d_x, s_y - d_y)
+                moving_agent_child = (s_x + d_x, s_y + d_y)
 
-                in_board = moving_agent_child[0] >= 0 and moving_agent_child[1] >= 0
-
+                in_board = moving_agent_child[0] >= 0 and moving_agent_child[1] >= 0 
+                in_bound = moving_agent_child[0] < len(chess_board) and moving_agent_child[1] < len(chess_board)
                 barrier = chess_board [s_x,s_y,self.dir_map[direction]]
                 
-                if in_board and not barrier:
+                if in_board and in_bound and not barrier:
 
-                    distance_to_enemy = math.hypot(moving_agent_child[0]  - adv_pos[0], moving_agent_child[1] - adv_pos[1])
-                    if (distance_to_enemy < math.hypot(best_move[0] - adv_pos[0], best_move[1] - adv_pos[1])):
+                    distance_to_enemy = math.hypot(moving_agent_child[0] - adv_pos[0], moving_agent_child[1] - adv_pos[1])
+                    if (distance_to_enemy <= math.hypot(best_move[0] - adv_pos[0], best_move[1] - adv_pos[1])):
                         best_move = moving_agent_child
             
-            moving_agent_parent = best_move
+            if best_move != moving_agent_parent:
+                moving_agent_parent = best_move
             k = k + 1
 
         my_pos = moving_agent_child
 
-        if (adv_pos[0] == my_pos[0] + 1 and adv_pos[1] == my_pos[1]):
+        # barrier selection
+
+        e_down = adv_pos[0] == my_pos[0] + 1 and adv_pos[1] == my_pos[1]
+        e_up = adv_pos[0] == my_pos[0] - 1 and adv_pos[1] == my_pos[1]
+        e_right = adv_pos[0] == my_pos[0] and adv_pos[1] == my_pos[1] + 1
+        e_left = adv_pos[0] == my_pos[0] and adv_pos[1] == my_pos[1] - 1
+
+        if e_down and not chess_board [my_pos[0],my_pos[1], self.dir_map["d"]]:
             barrier = self.dir_map["d"]
         
-        elif (adv_pos[0] == my_pos[0] - 1 and adv_pos[1] == my_pos[1]):
+        elif e_up and not chess_board [my_pos[0],my_pos[1], self.dir_map["u"]]:
             barrier = self.dir_map["u"]
 
-        elif (adv_pos[0] == my_pos[0] and adv_pos[1] == my_pos[1] + 1):
+        elif e_right and not chess_board [my_pos[0],my_pos[1], self.dir_map["r"]]:
             barrier = self.dir_map["r"]
         
-        elif (adv_pos[0] == my_pos[0] and adv_pos[1] == my_pos[1] - 1):
+        elif e_left and not chess_board [my_pos[0],my_pos[1], self.dir_map["l"]]:
             barrier = self.dir_map["l"]
         
         else:
+            print("choosing random barrier")
             valid_barrier = (0,1,2,3)
             cur_bar = chess_board [my_pos[0],my_pos[1]]
             valid_barrier = [i for i, cur_bar in enumerate(valid_barrier) if cur_bar]
             barrier = random.choice(valid_barrier)
-        
-        
 
-        """
-        Implement the step function of your agent here.
-        You can use the following variables to access the chess board:
-        - chess_board: a numpy array of shape (x_max, y_max, 4)
-        - my_pos: a tuple of (x, y)
-        - adv_pos: a tuple of (x, y)
-        - max_step: an integer
-
-        You should return a tuple of ((x, y), dir),
-        where (x, y) is the next position of your agent and dir is the direction of the wall
-        you want to put on.
-
-        Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
-        """
-        # dummy return
         return my_pos, barrier
